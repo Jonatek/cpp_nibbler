f477b3a701015e71862548c1a08fa3bc97aa0fdb
han_d <han_d@epitech.eu>
2015-03-24 14:43:50 +0100

    norme noms de fichier

---
+++ File.cpp
@@ -0,0 +1,25 @@
+//
+// File.cpp for File.cpp in /home/han_d/Github/cpp_nibbler
+// 
+// Made by Daniel Han
+// Login   <han_d@epitech.net>
+// 
+// Started on  Tue Mar 24 14:25:58 2015 Daniel Han
+// Last update Tue Mar 24 14:36:51 2015 Daniel Han
+//
+
+File::File()
+{
+}
+
+~File()
+{
+}
+
+File(File const &)
+{
+}
+
+File &operator=(File const &)
+{
+}
--- Map.cpp
+++ Map.cpp
@@ -5,13 +5,23 @@
 // Login   <saysan_j@epitech.net>
 // 
 // Started on  Tue Mar 24 13:21:23 2015 Jean-Paul SAYSANA
-// Last update Tue Mar 24 14:05:40 2015 Jean-Paul SAYSANA
+// Last update Tue Mar 24 14:36:28 2015 Daniel Han
 //
 
 #include "Map.hh"
 
 Map::Map(int x, int y)
 {
-  
 }
 
+Map::~Map()
+{
+}
+
+Map::Map(Map const & m)
+{
+}
+
+Map & Map::operator=(Map const & m)
+{
+}
