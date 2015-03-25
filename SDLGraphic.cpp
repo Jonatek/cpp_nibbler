ff47213d46f77a812ea993cf0a3544abe32d2847
quach_a <quach_a@epitech.eu>
2015-03-25 22:52:16 +0100

    add loop for window, and esc key to quit

--- SDLGraphic.cpp
+++ SDLGraphic.cpp
@@ -5,7 +5,7 @@
 // Login  <jonathan.quach@epitech.eu>
 // 
 // Started on  Wed Mar 25 21:21:53 2015 Jonathan Quach
-// Last update Wed Mar 25 22:28:22 2015 Jonathan Quach
+// Last update Wed Mar 25 22:49:49 2015 Jonathan Quach
 //
 
 #include "SDLGraphic.hpp"
@@ -33,6 +33,18 @@ void SDLGraphic::createWindow(const int &x, const int &y)
 
   if ((_win = SDL_CreateWindow("Nibbler", 0, 0, x, y, SDL_WINDOW_SHOWN)) == NULL)
     throw ErrorException(std::string("Error SDL_CreateWindow : ") + SDL_GetError());
+
+  SDL_Event event;
+  bool	end = true;
+
+  while (end)
+    {
+      while (SDL_PollEvent(&event))
+	{
+	  if (event.key.keysym.sym == SDLK_ESCAPE)
+	    end = false;
+	}
+    }
 }
 
 void SDLGraphic::updateEvent()
