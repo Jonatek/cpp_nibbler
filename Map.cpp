5b14a2dec3169c4f2290df4401e8c7361dc2734a
han_d <han_d@epitech.eu>
2015-04-02 15:50:51 +0200

    Map updated

--- Map.cpp
+++ Map.cpp
@@ -1,9 +1,24 @@
+//
+// Map.cpp for Map.cpp in /home/han_d/Github/cpp_nibbler
+// 
+// Made by Daniel Han
+// Login   <han_d@epitech.net>
+// 
+// Started on  Thu Apr  2 15:42:47 2015 Daniel Han
+// Last update Thu Apr  2 15:48:29 2015 Daniel Han
+//
+
+#include <stdlib.h>
 #include "Map.hpp"
 
+//
+// Coplien Form
+//
 Map::Map(int caseX, int caseY)
 {
   this->_objects.clear();
   this->_caseX = caseX;
+  this->_caseY = caseY;
   initMap(caseX, caseY);
 }
 
@@ -24,7 +39,17 @@ Map::~Map()
   this->_objects.clear();
 }
 
+//
+// Init Map
+//
 void Map::initMap(int caseX, int caseY)
+{
+  initWall(caseX, caseY);
+  initSnake(caseX, caseY);
+  addRandomObject(FOOD);
+}
+
+void Map::initWall(int caseX, int caseY)
 {
   int	x;
 
@@ -56,9 +81,35 @@ void Map::initMap(int caseX, int caseY)
     }
 }
 
+void Map::initSnake(int caseX, int caseY)
+{
+  int	pos = (caseY / 2) * _caseX + (caseX / 2);
+
+  this->_objects[pos] = HEAD;
+  this->_objects[pos + 1] = BODY;
+  this->_objects[pos + 2] = BODY;
+}
+
+//
+// Get/Add/Remove Object
+//
 Map::ObjectType Map::getObject(int x, int y) const
 {
   return (this->_objects[y * this->_caseX + x]);
+}
+
+void Map::addRandomObject(ObjectType object)
+{
+  int	pos;
+  int	deathdoor = 4242;
+
+  pos = rand() % (this->_caseX + this->_caseY);
+  while (this->_objects[pos] == NONE)
+    {
+      pos = rand() % (this->_caseX + this->_caseY);
+      --deathdoor;
+    }
+  this->_objects[pos] = object;
 }
 
 bool Map::addObject(int x, int y, ObjectType object)
@@ -76,5 +127,13 @@ bool Map::addObject(int x, int y, ObjectType object)
 
 void Map::removeObject(int x, int y)
 {
-  this->_objects[y * this->_caseX + x] = NONE;
+  int	pos = y * this->_caseX + x;
+
+  if (this->_objects[pos] == FOOD)
+    {
+      addObject(x, y, FOOD);
+      this->_objects[pos] = NONE;
+    }
+  else
+    this->_objects[pos] = NONE;
 }
--- Map.hpp
+++ Map.hpp
@@ -5,7 +5,7 @@
 // Login   <han_d@epitech.net>
 // 
 // Started on  Wed Apr  1 14:23:42 2015 Daniel Han
-// Last update Wed Apr  1 15:14:13 2015 Daniel Han
+// Last update Thu Apr  2 15:44:51 2015 Daniel Han
 //
 
 #ifndef MAP_HPP_
@@ -15,27 +15,50 @@
 
 class Map
 {
+  //
+  // Enum for ObjectType
+  //
 public:
   enum ObjectType
     {
       NONE,
       WALL,
+      HEAD,
+      BODY,
       FOOD
     };
 
+  //
+  // Attributs
+  //
 private:
   std::vector<ObjectType> _objects;
   int _caseX;
+  int _caseY;
 
+  //
+  // Coplien Form
+  //
 public:
   Map(int, int);
   Map(Map const &);
   Map & operator=(Map const &);
   ~Map();
 
+  //
+  // Init Map
+  //
 public:
   void initMap(int, int);
+  void initWall(int, int);
+  void initSnake(int, int);
+
+  //
+  // Get/Add/Remove Object
+  //
+public:
   ObjectType getObject(int, int) const;
+  void addRandomObject(ObjectType);
   bool addObject(int, int, ObjectType);
   void removeObject(int, int);
 
